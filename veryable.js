30eb1043cdb1218c8257ad13e14558d9c413af5c
sohanur00 <you@example.com>
2023-07-29 13:20:40 +0600

    string and boolean

---
+++ veryable.js
@@ -0,0 +1,16 @@
+// very+able=veryable
+
+// numeric
+var price =11;
+var age =17;
+var tempereture =31;
+var school =1;
+var many =101;
+
+// string
+var person = "sohan sohan";
+var location = "singra natore";
+
+// boolean (true fals)
+var seriou = true;
+var fullmsrks =true;
